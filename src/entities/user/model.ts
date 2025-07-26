4f2cd9573f907fc59e5b4804371bdeff1534797d
sinzem <sinzem@gmail.com>
2025-07-26 17:09:50 +0300

    add_tables_settings-migrations

--- src/entities/user/model.ts
+++ src/entities/user/model.ts
@@ -26,9 +26,9 @@ export const UserProgress = sequelize.define('UserProgress', {
     notification: {type: DataTypes.INTEGER, defaultValue: 0},
 });
 
-UserData.hasOne(UserProgress);
-UserProgress.belongsTo(UserData);
+UserData.hasOne(UserProgress, {foreignKey: 'userId', onDelete: 'CASCADE'});
+UserProgress.belongsTo(UserData, {foreignKey: 'userId'});
 
-UserData.hasMany(CardData);
-CardData.belongsTo(UserData);
+UserData.hasMany(CardData, {foreignKey: 'userId', onDelete: 'CASCADE'});
+CardData.belongsTo(UserData, {foreignKey: 'userId'});
 
